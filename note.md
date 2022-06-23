b7ba91d5b401f30c95c672f95c07b1fe652c8d4c
chishuai12 <chishuai12@163.com>
2022-06-24 00:57:32 +0900

    first commit. creat project

---
+++ note.md
@@ -0,0 +1,10 @@
+### 员工管理系统
+
+1.首页配置：注意点，所有页面的静态资源都需要使用thymeleaf接管。URL @{}
+```properties
+    # 关闭模板引擎的缓存
+    spring.thymeleaf.cache=false
+    
+    # 设置虚拟目录
+    server.servlet.context-path=/kuang
+```
---
+++ src/main/java/com/chi/dao/EmployeeDao.java
@@ -0,0 +1,55 @@
+package com.chi.dao;
+
+import com.chi.pojo.Department;
+import com.chi.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+// 员工Dao
+@Repository
+public class EmployeeDao {
+    // 模拟数据库中的数据
+    private static Map<Integer, Employee>  employees = null;
+
+    @Autowired
+    private  DepartmentDao departmentDao;
+
+    static {
+        employees = new HashMap<Integer, Employee>();
+        employees.put(1001,new Employee(1001,"AA","",0,new Department(101,"教学部")));
+        employees.put(1002,new Employee(1002,"BB","",1,new Department(102,"市场部")));
+        employees.put(1003,new Employee(1003,"CC","",0,new Department(103,"教研部")));
+        employees.put(1004,new Employee(1004,"DD","",1,new Department(104,"运营部")));
+        employees.put(1005,new Employee(1005,"EE","",0,new Department(105,"后勤部")));
+    }
+
+    // 主键自增
+    private static Integer initId = 1006;
+    // 增加一个员工
+    public void save(Employee employee){
+        if(employee.getId() == null){
+            employee.setId(initId++);
+        }
+        employee.setDepartment(departmentDao.getDepartmentById(employee.getDepartment().getId()));
+        employees.put(employee.getId(), employee);
+    }
+
+    // 查询员工
+    public Collection<Employee> getAll(){
+        return employees.values();
+    }
+
+    // 通过ID查询员工
+    public Employee getEmployeeById(Integer id){
+        return employees.get(id);
+    }
+
+    // 删除员工通过ID
+    public void delete(Integer id){
+        employees.remove(id);
+    }
+}
