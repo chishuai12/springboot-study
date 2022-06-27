dedc77ed72944e455fd0d0cd6fa0a5966a91a485
chishuai12 <chishuai12@163.com>
2022-06-27 23:34:52 +0900

    update Add employee

--- src/main/java/com/chi/config/EmployeeController.java
+++ src/main/java/com/chi/config/EmployeeController.java
@@ -1,11 +1,16 @@
 package com.chi.config;
 
+import com.chi.dao.DepartmentDao;
 import com.chi.dao.EmployeeDao;
+import com.chi.pojo.Department;
 import com.chi.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
 
@@ -15,6 +20,9 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @RequestMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
@@ -22,5 +30,22 @@ public class EmployeeController {
         return "emp/list";
     }
 
+    @GetMapping("/emp")
+    public String toAddpage(Model model){
+        // 查出所有的部门信息
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee, Model model){
+        // 添加操作 farward
+        // 调用底层保存员工信息
+        System.out.println("employee=>"+employee);
+        employeeDao.save(employee);
+
+        return "redirect:/emps";
+    }
 
 }
